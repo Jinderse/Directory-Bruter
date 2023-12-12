d6a78ef7e8c8ec934c0982dc8b64ef8982572bcd
Jinderse <150909797+Jinderse@users.noreply.github.com>
2023-12-12 18:49:42 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import requests
+from colorama import Fore
+try:
+    
+    dirin = input(Fore.CYAN+"Enter dirlist : ")
+
+    dirlist = open(dirin).read().split()
+    print(Fore.YELLOW+"https://web name/")
+    url = input(Fore.YELLOW+"Enter Your URL : ")
+    
+    for i in dirlist:
+        http = requests.get(url+i).status_code
+        if http == 200:
+            print(Fore.GREEN+" [+] There is >>>>> "+i)
+            break
+        
+        else:
+            print(Fore.RED+"[-] Does not exist >>>>>> "+i)
+        
+    
+except FileNotFoundError:
+    print("please Enter valid dyrectory list")
+except requests.exceptions.MissingSchema:
+    print("please Enter valid URL")
